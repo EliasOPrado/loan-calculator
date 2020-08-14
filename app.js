88757b3bfae2f782d51b75b88c85a21aa19361fc
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-08-14 20:31:03 +0100

    Added interest calculation total payment and monthly payment calculation

---
+++ app.js
@@ -0,0 +1,38 @@
+// listen for submit
+// get the loan-form and its input and add event listener when submitted with 
+//calculateResults function as action
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// calculate results
+function calculateResults(e){
+    // UI vars
+    // gets the first input id = amount
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    // from results
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    // to make decimal
+    const principal = parseFloat(amount.value);
+    // get the interest amount and divide by hundred and year
+    const calculateInterest = parseFloat(interest.value) / 100 / 12;
+    // calculate payments
+    const calculatePayments = parseFloat(years.value) * 12;
+
+    // monthly payments
+    const x = Math.pow(1 + calculateInterest, calculatePayments);
+    const monthly = (principal * x * calculateInterest)/(x-1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatePayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatePayments)- principal).toFixed(2);
+    }else{
+        console.log('Please check your numbers.')
+    }
+
+    e.preventDefault();
+}
